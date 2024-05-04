881b434f4f0dfa2e001679d604a66fb6ad113c83
sergiohardov <sergiohardov@gmail.com>
2024-05-04 23:32:08 +0300

    Add handle validation errors

--- controllers/User.js
+++ controllers/User.js
@@ -1,14 +1,9 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import { validationResult } from "express-validator";
 import UserModel from "../models/User.js";
 
 export const register = async (req, res) => {
   try {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) return res.status(400).json(errors.array());
-
     const password = req.body.password;
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
@@ -46,10 +41,6 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
   try {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) return res.status(400).json(errors.array());
-    
     const user = await UserModel.findOne({ email: req.body.email });
 
     if (!user) {
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import multer from "multer";
 import { registerValidation, loginValidation } from "./validations/auth.js ";
 import checkAuth from "./utils/checkAuth.js";
 import * as UserController from "./controllers/User.js";
+import handleValidationErrors from "./utils/handleValidationErrors.js";
 
 mongoose
   .connect(
@@ -28,8 +29,18 @@ const upload = multer({ storage });
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
-app.post("/auth/register", registerValidation, UserController.register);
-app.post("/auth/login", loginValidation, UserController.login);
+app.post(
+  "/auth/register",
+  registerValidation,
+  handleValidationErrors,
+  UserController.register
+);
+app.post(
+  "/auth/login",
+  loginValidation,
+  handleValidationErrors,
+  UserController.login
+);
 app.get("/auth/me", checkAuth, UserController.me);
 app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
   res.json({
